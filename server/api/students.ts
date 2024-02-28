75e1ef48e4a618b3c6ab4d4acdb16ee8ae9bb405
NoahOnFyre <noahonfyre@gmail.com>
2024-02-28 17:17:29 +0100

    feat: add students api endpoint.

---
+++ server/api/students.ts
@@ -0,0 +1,33 @@
+export default defineEventHandler(async event => {
+    return {
+        students: [
+            "Dominik",
+            "Hussain",
+            "Halil",
+            "Simon",
+            "Daniel",
+            "Massih",
+            "Lea",
+            "Manuel",
+            "Mikail",
+            "Asia",
+            "Elina",
+            "Carolina",
+            "Lana",
+            "Luca",
+            "Lorena",
+            "Linus",
+            "Enrico",
+            "Felix",
+            "Laurin",
+            "Emina",
+            "Naz Fatma",
+            "Jannik",
+            "Christian",
+            "Elias",
+            "Jeremy",
+            "Mia",
+            "Noah"
+        ]
+    }
+})
