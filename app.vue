120b6273a1475687bfde0373b212aef4ea958844
NoahOnFyre <noahonfyre@gmail.com>
2024-02-28 16:33:28 +0100

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+
+</template>
+
+<script setup>
+const { data, pending, error, refresh } = await useFetch("https://arche.webuntis.com/WebUntis/api/rest/extern/v1/students/{{id}}")
+console.log()
+</script>
