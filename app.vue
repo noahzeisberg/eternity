548bbc5edc899b1fcd60fbe2ab9fa342ac283ed0
NoahOnFyre <noahonfyre@gmail.com>
2024-02-28 16:33:56 +0100

    fix: remove unused code

--- app.vue
+++ app.vue
@@ -3,6 +3,5 @@
 </template>
 
 <script setup>
-const { data, pending, error, refresh } = await useFetch("https://arche.webuntis.com/WebUntis/api/rest/extern/v1/students/{{id}}")
-console.log()
+
 </script>
