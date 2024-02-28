c23b7c901b85eb7af516a7f9ff274e6e442747f9
NoahOnFyre <noahonfyre@gmail.com>
2024-02-28 17:18:00 +0100

    feat: add api endpoint connection for testing.

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <template>
-
+  <button @click="makeApiCall">Test</button>
+  <input v-model="number" type="number">
+  <span>{{ text }}</span>
 </template>
 
 <script setup>
+const text = ref("No information")
+const number = ref(0)
 
+async function makeApiCall() {
+  const data = await $fetch("/api/students", {
+    method: "GET"
+  })
+
+  text.value = data["students"][number.value]
+}
 </script>
