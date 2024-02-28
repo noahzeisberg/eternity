7dd24a9325f68d7a38a8427ba43d92a94e927719
NoahOnFyre <noahonfyre@gmail.com>
2024-02-28 19:03:05 +0100

    feat: add real logic and calculations for production use.

--- app.vue
+++ app.vue
@@ -1,18 +1,38 @@
 <template>
-  <button @click="makeApiCall">Test</button>
-  <input v-model="number" type="number">
-  <span>{{ text }}</span>
+  <div class="flex flex-col">
+    <h1>Diese Woche:</h1>
+    <span>{{ student1 }}</span>
+    <span>{{ student2 }}</span>
+  </div>
 </template>
 
 <script setup>
-const text = ref("No information")
-const number = ref(0)
+const student1 = ref("")
+const student2 = ref("")
 
-async function makeApiCall() {
-  const data = await $fetch("/api/students", {
+let currentCycle
+const week = getCurrentWeekOfYear()
+const { students, length } = await getStudents()
+const studentCount = length
+
+currentCycle = week > studentCount ? week - studentCount : week
+student1.value = students[currentCycle]
+
+const midIndex = Math.floor(currentCycle + studentCount / 2)
+student2.value = students[midIndex % studentCount]
+
+function getCurrentWeekOfYear() {
+  const now = new Date()
+  const startOfYear = new Date(now.getFullYear(), 0, 0)
+  const diff = now - startOfYear
+  const oneWeekInMillis = 1000 * 60 * 60 * 24 * 7
+  return Math.floor(diff / oneWeekInMillis)
+}
+
+async function getStudents() {
+  const { students } = await $fetch("/api/students", {
     method: "GET"
   })
-
-  text.value = data["students"][number.value]
+  return { students, length: students.length }
 }
 </script>
