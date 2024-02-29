37a41a46f0387929d2f5bb8b94e966f112e3ac98
NoahOnFyre <noahonfyre@gmail.com>
2024-02-29 20:40:07 +0100

    feat: add placeholder to several pages.

---
+++ components/placeholder.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col justify-center items-center text-center h-96 w-full border-4 border-dashed border-zinc-200 rounded-3xl mb-6 p-12">
+    <h1 class="font-bold">Hier ist noch nichts!</h1>
+    <span class="text-zinc-500 font-medium">Mehr Inhalt kann in Zukunft hinzugefügt werden.</span>
+  </div>
+</template>
