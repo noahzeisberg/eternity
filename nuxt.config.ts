57a23476b44017da54fc8705f1d93f181bb9e181
NoahOnFyre <noahonfyre@gmail.com>
2024-02-29 20:02:28 +0100

    fix: add more config to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
-  modules: [
+    devtools: {
+        enabled: true
+    },
+    modules: [
       '@nuxtjs/tailwindcss'
-  ],
+    ],
+    app: {
+        baseURL: "/unity/"
+    },
+    ssr: false,
     $production: {
       nitro: {
           storage: {
               dataStorage: {
-                  driver: "redis"
+                  driver: "memory"
               }
           }
       }
