c2679f66f0ff36f14b0a7fa5ee8b9fbdc21e55cf
NoahOnFyre <noahonfyre@gmail.com>
2024-02-29 19:35:55 +0100

    feat: add footer and navbar links

---
+++ components/footer/link.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink :to="to" class="text-zinc-700 hover:text-zinc-900"><slot></slot></NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  to: {
+    type: String,
+    required: false,
+    default: "#"
+  }
+})
+</script>
