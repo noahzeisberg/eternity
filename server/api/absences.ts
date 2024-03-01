eb6392473eff16d7d638bf1ed48bca5ef2ec91fb
NoahOnFyre <noahonfyre@gmail.com>
2024-03-02 00:01:55 +0100

    feat: remove absences.ts

--- server/api/absences.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async event => {
-    assertMethod(event, "POST")
-    const storage = useStorage("dataStorage")
-    const body = event._requestBody?.toString()
-
-})
