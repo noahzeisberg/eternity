4c3947dc3e8fdd0ad3960ce25bfb6bac65248bc5
NoahOnFyre <noahonfyre@gmail.com>
2024-03-02 23:32:54 +0100

    feat: sync with image title update.

--- components/about/profile.vue
+++ components/about/profile.vue
@@ -1,16 +1,8 @@
 <template>
-  <NuxtLink v-if="github" :to="`https://github.com/${username}`" class="flex items-center gap-3 my-2">
-    <img :src="`/${username}.webp`" class="rounded-full w-10 h-10" :alt="displayName">
+  <div class="flex items-center gap-3 my-2">
+    <img :src="`/${picture}.webp`" class="rounded-full w-10 h-10 active:scale-[5]" :alt="displayName">
     <div class="flex flex-col">
-      <span class="text-sm font-semibold">{{ displayName }}</span>
-      <span class="text-sm text-zinc-400">{{ task }}</span>
-    </div>
-  </NuxtLink>
-
-  <div v-else class="flex items-center gap-3 my-2">
-    <img :src="`/${username}.webp`" class="rounded-full w-10 h-10" :alt="displayName">
-    <div class="flex flex-col">
-      <span class="text-sm font-semibold">{{ displayName }}</span>
+      <span>{{ displayName }}</span>
       <span class="text-sm text-zinc-400">{{ task }}</span>
     </div>
   </div>
@@ -18,12 +10,8 @@
 
 <script setup>
 const props = defineProps({
-  username: String,
+  picture: String,
   displayName: String,
-  task: String,
-  github: {
-    type: Boolean,
-    default: false
-  }
+  task: String
 })
 </script>
