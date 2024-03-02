a48fca4d8182f35e32b18738aad523dcb4250131
NoahOnFyre <noahonfyre@gmail.com>
2024-03-02 18:01:16 +0100

    feat: add error page.

---
+++ error.vue
@@ -0,0 +1,27 @@
+<template>
+  <UnityPage page-title="Seite nicht gefunden.">
+    <Outline>
+      <Card>
+        <template #header>
+          <h1 class="font-semibold">Diese Seite existiert nicht!</h1>
+        </template>
+
+        <span>
+          Falls du glaubst, dass dies ein Fehler ist, <NuxtLink class="text-blue-700" to="mailto://noah.zeisberg@heinrichboell.schule">kontaktiere uns</NuxtLink>.
+        </span>
+
+        <template #footer>
+          <NuxtLink class="text-sm text-zinc-400">Zurück zur Übersicht</NuxtLink>
+        </template>
+      </Card>
+    </Outline>
+  </UnityPage>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
