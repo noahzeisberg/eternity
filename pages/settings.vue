f25ea1800e40b57143af09fb1a884052889dcb15
NoahOnFyre <noahonfyre@gmail.com>
2024-03-02 23:32:11 +0100

    feat: add setting sections.

--- pages/settings.vue
+++ pages/settings.vue
@@ -3,13 +3,34 @@
     <Outline>
       <Card>
         <template #header>
+          <h1 class="font-semibold">Präferenzen</h1>
+        </template>
+        Optionen werden bald hinzugefügt.
+      </Card>
+
+      <Card>
+        <template #header>
+          <h1 class="font-semibold">Erscheinungsbild</h1>
+        </template>
+        Optionen werden bald hinzugefügt.
+      </Card>
+
+      <Card>
+        <template #header>
+          <h1 class="font-semibold">Verhalten</h1>
+        </template>
+        Optionen werden bald hinzugefügt.
+      </Card>
+
+      <Card>
+        <template #header>
           <h1 class="font-semibold">Contributors</h1>
         </template>
-        <AboutProfile display-name="Noah Zeisberg" task="Core Developer" username="noahzeisberg" github/>
-        <AboutProfile display-name="Luca Peter" task="Datenverarbeitung & Ideen" username="lucaonfyre" github/>
-        <AboutProfile display-name="Dominik Bauer" task="Datenverarbeitung & Ideen" username="medomi106" github/>
-        <AboutProfile display-name="Massih Haschemi" task="Ideen & Maintenance" username="massih"/>
-        <AboutProfile display-name="Elias Wardak" task="Ideen" username="elias795" github/>
+        <AboutProfile display-name="Noah Zeisberg" task="Core Developer" picture="noah"/>
+        <AboutProfile display-name="Luca Peter" task="Datenverarbeitung & Ideen" picture="luca"/>
+        <AboutProfile display-name="Dominik Bauer" task="Datenverarbeitung & Ideen" picture="dominik"/>
+        <AboutProfile display-name="Massih Haschemi" task="Ideen & Datenkollektion" picture="massih"/>
+        <AboutProfile display-name="Elias Wardak" task="Ideen" picture="elias"/>
       </Card>
 
       <Card>
@@ -18,6 +39,9 @@
         </template>
         <p>Version: master@<NuxtLink :to="commitLink" class="text-zinc-400 underline">({{ commitSha }})</NuxtLink></p>
         <p>Letztes Update: {{ commitTimeString }}</p>
+        <template #footer>
+          <NuxtLink to="https://github.com/noahzeisberg/unity" class="text-zinc-400">GitHub Repository</NuxtLink>
+        </template>
       </Card>
 
       <NuxtLink to="mailto://noah.zeisberg@heinrichboell.schule" class="text-center w-full py-3 bg-blue-700 text-zinc-50 rounded-lg">Probleme melden</NuxtLink>
