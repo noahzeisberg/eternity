14111a13484de0dd40ca5bb03814d2768bf0e35c
NoahOnFyre <noahonfyre@gmail.com>
2024-03-03 11:12:15 +0100

    style: remove scale class

--- components/about/profile.vue
+++ components/about/profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-3 my-2">
-    <img :src="`/${picture}.webp`" class="rounded-full w-10 h-10 active:scale-[5]" :alt="displayName">
+    <img :src="`/${picture}.webp`" class="rounded-full w-10 h-10" :alt="displayName">
     <div class="flex flex-col">
       <span>{{ displayName }}</span>
       <span class="text-sm text-zinc-400">{{ task }}</span>
