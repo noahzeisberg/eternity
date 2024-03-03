b43882fbb46dc7c87608352a1e93400a1c53baf1
NoahOnFyre <noahonfyre@gmail.com>
2024-03-03 14:09:48 +0100

    feat: add calendar view

---
+++ pages/calendar.vue
@@ -0,0 +1,88 @@
+<template>
+  <UnityPage page-title="Kalender">
+    <Outline v-if="true">
+      <Disclosure v-slot="{ open }" v-for="month in exams">
+        <Card v-if="month.exams.length !== 0">
+          <template #header>
+            <DisclosureButton class="text-start">
+              <h1 class="font-semibold">{{ month.name }}</h1>
+            </DisclosureButton>
+          </template>
+
+          <template v-if="open" #footer>
+            <DisclosurePanel class="flex flex-col" static>
+              <span v-for="exam in month.exams"><span class="text-zinc-400">{{ exam.date }}. {{ month.name }}</span> - {{ exam.name }}</span>
+            </DisclosurePanel>
+          </template>
+        </Card>
+      </Disclosure>
+    </Outline>
+
+    <TabGroup v-else>
+      <TabList class="flex gap-1.5 p-1.5 m-3 rounded-lg bg-zinc-100">
+        <template v-for="month in exams">
+          <Tab v-if="month.exams.length !== 0" class="text-sm outline-none ui-selected:bg-white font-medium ui-selected:text-blue-700 w-full py-2 rounded-md ui-selected:shadow">{{ month.name }}</Tab>
+        </template>
+      </TabList>
+
+      <TabPanels>
+        <template v-for="month in exams">
+          <TabPanel v-if="month.exams.length !== 0">
+            <Outline>
+              <Card v-for="exam in month.exams">
+                <template #header>
+                  <h1 class="font-semibold">{{ exam.date }}. {{ month.name }}. {{ month.year }} &middot; <span class="font-normal">{{ exam.name }}</span></h1>
+                </template>
+
+                <template v-if="exam.teacher.length !== 0" #footer>
+                <span class="flex gap-1">
+                  <span v-for="teacher in exam.teacher">{{ teacher }}</span>
+                </span>
+                </template>
+              </Card>
+            </Outline>
+          </TabPanel>
+        </template>
+      </TabPanels>
+    </TabGroup>
+  </UnityPage>
+</template>
+
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels
+} from "@headlessui/vue";
+
+const date = ref(new Date())
+const exams = await getExams()
+
+async function getExams() {
+  const { exams } = await $fetch("/api/exams")
+  return exams
+}
+
+function getDate(d) {
+  const date = new Date(d)
+  return date.getDate() + ". " + [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+  ][date.getMonth()] + " " + date.getFullYear()
+}
+</script>
