b0063ede165394611c8bbd65562a3b0cf75277a6
NoahOnFyre <noahonfyre@gmail.com>
2024-03-03 14:09:37 +0100

    refactor: change tab name.

--- components/quick/nav.vue
+++ components/quick/nav.vue
@@ -15,7 +15,7 @@
       </template>
     </QuickLink>
 
-    <QuickLink title="Plan" to="/timetable">
+    <QuickLink title="Fächer" to="/timetable">
       <template #active>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
           <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
@@ -28,7 +28,7 @@
       </template>
     </QuickLink>
 
-    <QuickLink title="Termine" to="/calendar">
+    <QuickLink title="Kalender" to="/calendar">
       <template #active>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
           <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
