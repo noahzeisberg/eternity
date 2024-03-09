93b9bccce3fd1d0ca6141963762ebbc0aaa9d1ce
NoahOnFyre <noahonfyre@gmail.com>
2024-03-09 14:41:56 +0100

    chore(deps): switch back due to compatibility issues.

--- error.vue
+++ error.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import type { NuxtError } from '#app'
+<template>
+  <LiquidPage :page-title="props.error.statusMessage.split(':')[0]">
+    <LiquidOutline>
+      <div class="flex items-center justify-center text-center flex-col my-6">
+        <LiquidTitle class="text-blue-700 font-bold text-5xl mb-3">{{ props.error.statusCode }}</LiquidTitle>
+        <LiquidTitle>{{ props.error.statusMessage.split(':')[0] }}</LiquidTitle>
+        <LiquidDisclaimer>Ein Stacktrace befindet sich unten.</LiquidDisclaimer>
+        <LiquidDisclaimer class="mx-6 my-12" accent>{{ props.error.stack }}</LiquidDisclaimer>
+      </div>
+    </LiquidOutline>
+  </LiquidPage>
+</template>
 
-const props = defineProps({
-  error: Object as () => NuxtError
-})
-
-useRouter().push("/error")
+<script setup>
+const props = defineProps({error: {}})
 </script>
