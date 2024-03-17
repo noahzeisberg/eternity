880d4905f0bd14f17155be59cdc74e1a8469a72b
NoahOnFyre <noahonfyre@gmail.com>
2024-03-17 02:35:42 +0100

    style: fix component styles.

--- components/form/button.vue
+++ components/form/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="accent && 'bg-zinc-900 ring-1 ring-zinc-800 ring-inset'" class="text-sm bg-blue-700 text-zinc-50 px-6 py-2.5 rounded-md shadow font-semibold">
+  <button :class="accent && 'bg-zinc-900 ring-1 ring-zinc-800 ring-inset'" class="max-w-md text-sm bg-blue-700 text-zinc-50 px-6 py-2.5 rounded-md shadow font-semibold">
     <slot></slot>
   </button>
 </template>
--- components/form/input.vue
+++ components/form/input.vue
@@ -1,5 +1,5 @@
 <template>
-  <input :placeholder="placeholder" class="text-sm px-4 py-2.5 ring-1 ring-zinc-800 ring-inset rounded-md shadow bg-zinc-900 placeholder-zinc-600 outline-none focus:outline-2 focus:outline-blue-700 duration-300 ease-in-out" :type="type" v-model="model">
+  <input :placeholder="placeholder" class="text-sm px-4 py-2.5 ring-1 ring-zinc-800 ring-inset rounded-md shadow bg-zinc-900 placeholder-zinc-600 max-w-md outline-none focus:outline-2 focus:outline-blue-700 duration-300 ease-in-out" :type="type" v-model="model">
 </template>
 
 <script setup>
