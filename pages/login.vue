235fc7eb4ad295e59972fb53296b564851f20e4c
NoahOnFyre <noahonfyre@gmail.com>
2024-03-17 02:35:29 +0100

    style: fix layout issues in login page.

--- pages/login.vue
+++ pages/login.vue
@@ -1,29 +1,15 @@
 <template>
-  <div class="flex flex-col justify-between text-center items-stretch p-6 h-screen">
-    <div class="flex flex-col mt-16">
+  <div class="flex flex-col justify-center text-center gap-12 items-stretch p-6 h-screen">
+    <div class="flex flex-col">
       <LiquidTitle class="text-2xl font-bold">Eternity Login</LiquidTitle>
       <LiquidDisclaimer accent>Active Development Preview</LiquidDisclaimer>
     </div>
 
     <div class="flex flex-col gap-3">
-      <div class="flex flex-col gap-1">
-        <div class="flex justify-between items-center">
-          <LiquidText>Benutzer</LiquidText>
-          <LiquidDisclaimer accent></LiquidDisclaimer>
-        </div>
-        <FormInput @click="resetMsg" type="text" v-model="username"></FormInput>
-      </div>
-      <div class="flex flex-col gap-1">
-        <div class="flex justify-between items-center">
-          <LiquidText>Passwort</LiquidText>
-          <LiquidDisclaimer @click="sendPasswordRecovery" accent><button>Passwort vergessen?</button></LiquidDisclaimer>
-        </div>
-        <FormInput @click="resetMsg" type="password" v-model="password"></FormInput>
-      </div>
+      <FormInput placeholder="Benutzer" @click="resetMsg" type="text" v-model="username"></FormInput>
+      <FormInput placeholder="Passwort" @click="resetMsg" type="password" v-model="password"></FormInput>
       <div v-if="msg !== ''" class="text-sm bg-red-700/15 ring-1 ring-red-700/30 py-2.5 rounded-md">{{ msg }}.</div>
-    </div>
-
-    <div class="flex flex-col gap-3">
+      <br>
       <FormButton @click="login">Anmelden</FormButton>
       <FormButton @click="signUp" accent>Registrieren</FormButton>
       <NuxtLink to="mailto://noah.zeisberg@heinrichboell.schule">
@@ -34,8 +20,6 @@
 </template>
 
 <script setup>
-import Button from "~/components/form/button.vue";
-
 const supabase = useSupabaseClient()
 const username = ref("")
 const password = ref("")
@@ -67,13 +51,6 @@ async function signUp() {
   if (error) msg.value = error.message
 }
 
-async function sendPasswordRecovery() {
-  const { email, isTeacherLogin } = parseMailAddress(username.value)
-  const { error } = await supabase.auth.resetPasswordForEmail(email)
-  msg.value = "Bitte überprüfe deinen Posteingang"
-  if (error) msg.value = error.message
-}
-
 async function resetMsg() {
   msg.value = ""
 }
@@ -85,7 +62,6 @@ function parseMailAddress(username) {
     email = username
   } else if(username.includes(".")) {
     email = username + "@heinrichboell.schule"
-    isTeacherLogin = false
   } else {
     email = username + "@hbs-hattersheim.de"
     isTeacherLogin = true
