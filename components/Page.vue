016c27e73e354e99f19cd0290f1c5136cfc5e113
NoahOnFyre <noahonfyre@gmail.com>
2024-04-16 15:34:35 +0200

    style: change icon

--- components/Page.vue
+++ components/Page.vue
@@ -1,7 +1,9 @@
 <template>
   <UCard class="rounded-none">
     <div class="flex justify-between items-center -my-2">
-      <ULink to="/settings"><UAvatar size="sm" :alt="useCookie('user.name').value"></UAvatar></ULink>
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="size-8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M11 8L3 16H11L11 8ZM13 16L21 8L13 8V16Z" fill="currentColor"/>
+      </svg>
       <h1 class="font-semibold">{{ title }}</h1>
       <UButton @click="isOpen = true" color="gray" variant="ghost" icon="i-heroicons-bars-3"></UButton>
     </div>
