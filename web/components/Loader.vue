057499f82d9da4e17c6e657d2540ad7c5f4c6136
NoahOnFyre <noahonfyre@gmail.com>
2024-04-26 17:03:08 +0200

    feat(components): add loader component.

---
+++ web/components/Loader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center items-center w-full py-6">
+    <UpdateIcon class="size-6 animate-spin"></UpdateIcon>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { UpdateIcon } from "@radix-icons/vue";
+</script>
