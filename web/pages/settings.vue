bc4d996555549e3af63356dcff51fd109cfba4af
NoahOnFyre <noahonfyre@gmail.com>
2024-04-26 18:24:58 +0200

    feat: add dummy layout to settings.

--- web/pages/settings.vue
+++ web/pages/settings.vue
@@ -1,5 +1,19 @@
 <template>
   <Page>
-
+    <Card>
+      <CardHeader>
+        <CardTitle>Allgemein</CardTitle>
+        <CardDescription>Test</CardDescription>
+      </CardHeader>
+      <CardFooter class="flex-col">
+        <div class="flex justify-between w-full">
+          <Label for="enabled"></Label>
+          <Switch></Switch>
+        </div>
+      </CardFooter>
+    </Card>
   </Page>
 </template>
+<script setup lang="ts">
+import {Switch} from "~/components/ui/switch";
+</script>
