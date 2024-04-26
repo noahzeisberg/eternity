bd7fd2f300184e16ae8573130869b29a99630f26
NoahOnFyre <noahonfyre@gmail.com>
2024-04-26 16:57:23 +0200

    style(shadcn): update sheet style

--- web/components/ui/sheet/index.ts
+++ web/components/ui/sheet/index.ts
@@ -10,7 +10,7 @@ export { default as SheetDescription } from './SheetDescription.vue'
 export { default as SheetFooter } from './SheetFooter.vue'
 
 export const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 dark:bg-zinc-950',
+  'fixed z-50 gap-4 border-zinc-200 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 dark:border-zinc-800 dark:bg-zinc-950',
   {
     variants: {
       side: {
