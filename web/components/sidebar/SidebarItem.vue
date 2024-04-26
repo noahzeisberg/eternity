84ee8089667023408af206b181cfee63d047b7e3
NoahOnFyre <noahonfyre@gmail.com>
2024-04-26 16:59:09 +0200

    feat: add sidebar item component.

---
+++ web/components/sidebar/SidebarItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink class="flex gap-3 items-center hover:bg-zinc-100 hover:dark:bg-zinc-900 px-5 py-1.5 rounded-md" active-class="bg-zinc-100 dark:bg-zinc-900" :to="to">
+    <slot></slot>
+    <h1>{{ name }}</h1>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  to: String,
+  name: String
+})
+</script>
