7b0d33df50b6d334a74e20aa2900843db0e7b8a0
NoahOnFyre <noahonfyre@gmail.com>
2024-04-26 17:02:46 +0200

    feat(pages): add logout page.

---
+++ web/pages/logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-screen h-screen bg-white dark:bg-zinc-950 flex flex-col items-center justify-center">
+    <Loader></Loader>
+    <h1>Abmelden...</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {toast} from "vue-sonner";
+
+const logout = async () => {
+  toast.info("Abmeldung erfolgt...")
+  const { error } = await useSupabaseClient().auth.signOut()
+  if (error) {
+    toast.error(error)
+  } else {
+    toast.success("Erfolgreich abgemeldet!")
+    navigateTo("/")
+  }
+}
+
+logout()
+</script>
