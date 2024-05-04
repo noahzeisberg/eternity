c363a7e02dbcb4cccfa50f3673c951c490a83821
NoahOnFyre <noahonfyre@gmail.com>
2024-05-04 21:28:51 +0200

    feat: add class page.

---
+++ web/pages/class.vue
@@ -0,0 +1,22 @@
+<template>
+  <Page>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="student in students">
+          <TableCell>
+            {{ student }}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </Page>
+</template>
+
+<script setup lang="ts">
+const { data: students } = useLazyAsyncData("students", () => $fetch("/api/students"))
+</script>
