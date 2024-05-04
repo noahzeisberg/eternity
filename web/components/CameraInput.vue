acf0f40bb5a93e2dbbe4054a30d4b25d244b1b83
NoahOnFyre <noahonfyre@gmail.com>
2024-05-04 21:28:38 +0200

    feat: add camera input component.

---
+++ web/components/CameraInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <Label class="h-9 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-300 border border-zinc-200 bg-white shadow-sm hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50" for="camera">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 mr-2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+    </svg>
+    <slot></slot>
+  </Label>
+  <Input id="camera" class="hidden" type="file" accept="image/*" @change="change" :capture="type"></Input>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  type: {
+    type: String,
+    default: "environment"
+  }
+})
+
+const model = defineModel()
+
+const change = (event: any) => {
+  model.value = event.target.files[0]
+}
+</script>
