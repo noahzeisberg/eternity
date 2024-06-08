9e60ee869f6aa5da674bad6daba7577e90187516
NoahOnFyre <noahonfyre@gmail.com>
2024-06-08 22:54:06 +0200

    fix: remove unnecessary testing section in settings page.

--- web/pages/settings.vue
+++ web/pages/settings.vue
@@ -12,19 +12,6 @@
 
     <Card>
       <CardHeader>
-        <CardTitle>Allgemein</CardTitle>
-        <CardDescription>Test</CardDescription>
-      </CardHeader>
-      <CardFooter class="flex-col">
-        <div class="flex justify-between w-full">
-          <Label for="enabled">Wert</Label>
-          <Switch></Switch>
-        </div>
-      </CardFooter>
-    </Card>
-
-    <Card>
-      <CardHeader>
         <CardTitle>Mitwirkende</CardTitle>
         <CardDescription>Eine Liste des Kernteams und wichtiger Personen bezüglich der Entwicklung.</CardDescription>
       </CardHeader>
