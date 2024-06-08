647a80f41622ee13b069a6bc32f7b3fda6c7bfa6
NoahOnFyre <noahonfyre@gmail.com>
2024-06-08 13:37:17 +0200

    feat: add contributors to setting page.

--- web/pages/settings.vue
+++ web/pages/settings.vue
@@ -2,6 +2,16 @@
   <Page>
     <Card>
       <CardHeader>
+        <CardTitle>Profil</CardTitle>
+        <CardDescription>Passe dein Profil an.</CardDescription>
+      </CardHeader>
+      <CardFooter class="flex-col">
+
+      </CardFooter>
+    </Card>
+
+    <Card>
+      <CardHeader>
         <CardTitle>Allgemein</CardTitle>
         <CardDescription>Test</CardDescription>
       </CardHeader>
@@ -12,9 +22,24 @@
         </div>
       </CardFooter>
     </Card>
+
+    <Card>
+      <CardHeader>
+        <CardTitle>Mitwirkende</CardTitle>
+        <CardDescription>Eine Liste des Kernteams und wichtiger Personen bezüglich der Entwicklung.</CardDescription>
+      </CardHeader>
+      <CardContent class="flex flex-col gap-1">
+        <Contributor name="Noah" role="Core Developer" link="https://noahzeisberg.github.io"></Contributor>
+        <Contributor name="Luca" role="Database Operations" link="https://steamcommunity.com/profiles/76561199210639380"></Contributor>
+        <Contributor name="Dominik" role="PR Manager & Technical Writer" link="https://github.com/medomi106"></Contributor>
+        <Contributor name="Massih" role="Quality Assurance" link=""></Contributor>
+        <Contributor name="Raziel" role="Technical Writer" link="https://"></Contributor>
+      </CardContent>
+    </Card>
   </Page>
 </template>
 
 <script setup lang="ts">
 import {Switch} from "~/components/ui/switch";
+import Contributor from "~/components/widgets/Contributor.vue";
 </script>
